379c99b4ba6de83918b7128a377ba72dace7ce8f
Christian <soft.techsofts@gmail.com>
2021-07-31 01:49:22 +0000

    Deploy project to GitHub repository

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal");
+
+    if(principal.value > 0 && principal.value != ""){
+        document.getElementById("resultText").hidden=false
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal.value * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
+
+        document.getElementById("amount").innerText = principal.value
+        document.getElementById("interest").innerText = rate
+        document.getElementById("result").innerText = interest.toString();
+        document.getElementById("year").innerText = year;
+    }else{
+        alert("Enter a positive number");
+        principal.focus();
+    }
 }
-        
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
